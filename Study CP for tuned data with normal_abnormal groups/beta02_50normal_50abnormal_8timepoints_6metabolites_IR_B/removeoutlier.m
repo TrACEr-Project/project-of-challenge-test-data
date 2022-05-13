88112bc068938abf1a7b9ff63cda996160a8544b
Lu-source <66491155+Lu-source@users.noreply.github.com>
2022-05-13 14:34:51 +0200

    Add files via upload

---
+++ Study CP for tuned data with normal_abnormal groups/beta02_50normal_50abnormal_8timepoints_6metabolites_IR_B/removeoutlier.m
@@ -0,0 +1,10 @@
+
+%%%%%% This code is to remove the outlier in the first mode 
+
+function Y=removeoutlier(Y,outlier_pid)
+
+[subject_notoutlier,index] = setdiff(str2num(Y.label{1}), outlier_pid);
+Y=Y(index,:,:);
+end
+
+
