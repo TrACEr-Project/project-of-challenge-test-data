b8bb271cc41128680cb5e539076e323a13294a88
Lu-source <66491155+Lu-source@users.noreply.github.com>
2022-10-17 09:11:02 +0200

    Add files via upload

---
+++ Data and code for the simulated part/code for the simulated data/Stability check code-simulated data/Disease B/compare_diffbeta_balance_unbalance_full.m
@@ -0,0 +1,31 @@
+clear all
+clc
+close all
+
+%% compute factor match score--unbalance vs. balance beta=0.2
+%  load factors
+load('Fac_CP4_perm_full_balance_beta02.mat','Fac_CP4_perm_full')
+Fac_CP4_perm_full_balance_beta02=Fac_CP4_perm_full;
+load('Fac_CP4_perm_full_unbalance_beta02.mat','Fac_CP4_perm_full')
+Fac_CP4_perm_full_unbalance_beta02=Fac_CP4_perm_full;
+Fac_CP4_perm_full_balance_beta02_meta_time=ktensor([1;1;1;1],Fac_CP4_perm_full_balance_beta02.U{2},Fac_CP4_perm_full_balance_beta02.U{3});
+Fac_CP4_perm_full_unbalance_beta02_meta_time=ktensor([1;1;1;1],Fac_CP4_perm_full_unbalance_beta02.U{2},Fac_CP4_perm_full_unbalance_beta02.U{3});
+score_meta_time_unbalance_beta02 = score(Fac_CP4_perm_full_unbalance_beta02_meta_time,Fac_CP4_perm_full_balance_beta02_meta_time,'lambda_penalty',false)
+
+
+
+
+%% compute factor match score--balance beta=0.4 vs. balance beta=0.2
+%  load factors
+load('Fac_CP5_perm_full_balance_beta04.mat','Fac_CP5_perm_full')
+Fac_CP5_perm_full_balance_beta04=Fac_CP5_perm_full;
+Fac_CP5_perm_full_balance_beta04_meta_time=ktensor([1;1;1;1;1],Fac_CP5_perm_full_balance_beta04.U{2},Fac_CP5_perm_full_balance_beta04.U{3});
+score_meta_time_balance_beta04 = score(Fac_CP5_perm_full_balance_beta04_meta_time,Fac_CP4_perm_full_balance_beta02_meta_time,'lambda_penalty',false)
+
+%% compute factor match score--unbalance beta=0.4 vs. balance beta=0.2
+% load factors
+load('Fac_CP5_perm_full_unbalance_beta04.mat','Fac_CP5_perm_full')
+Fac_CP5_perm_full_unbalance_beta04=Fac_CP5_perm_full;
+Fac_CP5_perm_full_unbalance_beta04_meta_time=ktensor([1;1;1;1;1],Fac_CP5_perm_full_unbalance_beta04.U{2},Fac_CP5_perm_full_unbalance_beta04.U{3});
+score_meta_time_unbalance_beta04 = score(Fac_CP5_perm_full_unbalance_beta04_meta_time,Fac_CP4_perm_full_balance_beta02_meta_time,'lambda_penalty',false)
+
